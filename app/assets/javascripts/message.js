127622d4a79929296b4815fc73edf2d86b7152c3
yousukekaneko <kaneko1987-0402@i.softbank.jp>
2018-05-21 19:38:40 +0900

    メッセージ送信機能の非同期通信

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,41 @@
+$(function(){
+  function buildHTML(message) {
+    var html = `<div class="message">
+<div class="upper-message">
+<div class="right-content__message3">
+  ${message.user_name}
+</div>
+<span class="right-content__date">
+  ${message.created_at}
+</span>
+</div>
+<div class="right-content__message4">
+<p class="lower-message__content">
+  ${message.content}
+</p>
+
+</div>
+</div>`
+    return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.right-content__body').append(html).animate({scrollTop: $('.right-content__body')[0].scrollHeight}, 450);
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+});
