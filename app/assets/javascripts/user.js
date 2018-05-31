c20b0bd98566fbffa3d3f5c6d6f969562ed2c834
yousukekaneko <kaneko1987-0402@i.softbank.jp>
2018-05-31 22:06:23 +0900

    コンフリクトの解消

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,17 +1,18 @@
 $(function() {
-  var noExist = $('#user-search-result');
+  var nameList = $('#user-search-result');
+  var memberSpace = $("#chat-member-space");
   function appendUser(user) {
     var html = `<div class='chat-group-user clearfix'>
                   <p class="chat-group-user__name">${ user.name }</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${ user.id } data-user-name=${ user.name }>追加</a>
                 </div>`
-    noExist.append(html);
+    nameList.append(html);
   }
   function appendNoUser(user) {
     var html = `<li>
                   <div class='chat-group-user clearfix'>${ user.name }</div>
                 </li>`
-    noExist.append(html);
+    nameList.append(html);
   }
   function appendUserResult(user_name, user_id) {
     var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user${ user_id }'>
@@ -19,7 +20,7 @@ $(function() {
                   <p class='chat-group-user__name'>${ user_name }</p>
                   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                 </div>`
-    return html;
+    memberSpace.append(html);
   }
   $("#user-search-field").on("keyup", function(){
     var input = $("#user-search-field").val();
@@ -33,7 +34,7 @@ $(function() {
       $("#user-search-result").empty();
       if (data.length !== 0) {
         data.forEach(function(user){
-          $("#user-search-result").append(appendUser(user));
+          appendUser(user);
         });
       }
       else {
@@ -47,10 +48,13 @@ $(function() {
   $("#user-search-result").on("click", ".chat-group-user__btn--add", function() {
     var barName = $(this).data('user-name')
     var barId =$(this).data('user-id')
-    $("#chat-member-space").append(appendUserResult(barName, barId));
+      appendUserResult(barName, barId);
     $(this).parent().remove()
   });
   $("#chat-member-space").on("click", ".chat-group-user__btn--remove", function() {
-    $("#chat-group-user-1").remove();
+    $(this).parent().remove();
+  });
+  $("#new_group").on("click", ".chat-group-user__btn--remove", function() {
+    $(".chat-group-current-user").remove();
   });
 });
