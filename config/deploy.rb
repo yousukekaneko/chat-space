d147943dfb9adb5d39da3ffb38047338db6d46bc
yousukekaneko <kaneko1987-0402@i.softbank.jp>
2018-06-04 21:27:27 +0900

    deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "chat-space"
-set :repo_url, "git@example.yousukekaneko/chat-space.git"
+set :repo_url, "git@github.com:yousukekaneko/chat-space.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
